7eeced06d941b3da11ddc2a284db810fd56b591b
Palexander21 <palexander2113@gmail.com>
2019-09-03 19:01:10 -0600

    Add equipment from the table page

--- routes/tables.js
+++ routes/tables.js
@@ -1,5 +1,6 @@
 let express = require('express');
 let router = express.Router();
+const { body, validationResult } = require('express-validator/check');
 const mongoose = require('mongoose');
 const users = mongoose.model('Users');
 const equipment = mongoose.model('Equipment');
@@ -21,6 +22,17 @@ router.get('/', function(req, res, next) {
         console.log(err);
         res.send('Sorry! Something went wrong.');
     });
+});
+
+router.post('/', async (req, res, next) => {
+    const errors = validationResult(req);
+    if (errors.isEmpty()) {
+        let equip = new equipment(req.body);
+        await equip.save();
+
+    }
+    res.redirect('tables');
+
 });
 
 module.exports = router;
