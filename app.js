f5f46c8686b4452970d3dc96d37dfb030aff9789
Paxton Alexander <palexander2113@gmail.com>
2019-10-03 09:40:30 -0600

    Update Logging method

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ let app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(logger('dev'));
+app.use(logger(':remote-addr [:date[web]] :method :url :status - :response-time ms'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
